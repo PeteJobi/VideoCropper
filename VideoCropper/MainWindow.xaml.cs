859a0cea84fa2adf48709e64f2398f85b72e4328
Peter Egunjobi <masterpdon@gmail.com>
2025-07-26 18:19:18 +0100

    Push to Github

---
+++ VideoCropper/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+using CropperLibrary;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Cropper
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public static Window Window;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window = this;
+            AppWindow.Resize(new SizeInt32(1800, AppWindow.Size.Height));
+            WindowFrame.Navigate(typeof(MainPage));
+        }
+    }
+}
